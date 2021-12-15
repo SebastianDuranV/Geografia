82b457cc3461139a0cb033d0773e98daded6c317
sebasrianDuran <sebastian.duran@alumnos.uach.cl>
2021-12-15 11:25:43 -0300

    listo

--- api.py
+++ api.py
@@ -50,11 +50,11 @@ def post(id,instrumento):
             print("guardado")        
 
         info = request.json['dato']
-        info = base64.b64decode(info)
+        infoA = base64.b64decode(info)
         image_result = open(directorio + 'static/monitoreoDinamico/' + id + '/ultima.jpg', 'wb') # create a writable image and write the decoding result
-        image_result.write(info)
+        image_result.write(infoA)
         image_result = open(directorio + 'static/monitoreoDinamico/' + id + '/' + request.json['data'] +'.jpg', 'wb') # create a writable image and write the decoding result
-        image_result.write(info)
+        image_result.write(infoA)
 
         generarVideo(id)
 
